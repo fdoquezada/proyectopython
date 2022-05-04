6eb4b407d3c9411d9a450d7019bde74a580706aa
fernandres <fdoquezadadpuno@gmail.com>
2022-05-04 02:27:00 -0400

    actualizacion

--- proyecto2/proyectotienda/tienda/models.py
+++ proyecto2/proyectotienda/tienda/models.py
@@ -1,21 +1,33 @@
+from tokenize import Comment
 from django.db import models
 
 
 
 class Cliente(models.Model):
-    rut = models.CharField(max_length=50,unique=True),
-    nombre = models.CharField(max_length=50),
-    apellidoPaterno = models.CharField(max_length=50),
-    apellidoMaterno = models.CharField(max_length=50),
-    fecaNacimiento = models.DateField(),
-    sexos = [ 
-        ('M', 'Masculino'),
-        ('F', 'Femenino'),
-    ]
-    sexo = models.CharField(max_length=1, choices=sexos, default=None),
-    direccion = models.CharField(max_length=50),
-    telefono= models.CharField(max_length=100),
-    email= models.EmailField(max_length=100),
-    
+    nombre=models.CharField(max_length=50)
+    apellidoPaterno=models.CharField(max_length=50)
+    apellidoMaterno=models.CharField(max_length=50)
+    fecaNacimiento=models.DateField()
+    direccion=models.CharField(max_length=50)
+    ciudad=models.CharField(max_length=50)
+    telefono=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100)
+
+    def _str_(self):
+        return self.nombre + " " + self.apellidoPaterno + " " + self.apellidoMaterno
+class Proveedores(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellidoPaterno=models.CharField(max_length=50)
+    apellidoMaterno=models.CharField(max_length=50)
+    fecaNacimiento=models.DateField()
+    direccion=models.CharField(max_length=50)
+    ciudad=models.CharField(max_length=50)
+    telefono=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100)    
+
+    def _str_(self):
+        return self.nombre + " " + self.apellidoPaterno + " " + self.apellidoMaterno
+
+
 # Create your models here.
 
